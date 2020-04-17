b197d76033c5a78712cca484ca2870d7cd8c59f9
Mohammad Salkini <eng.salkini@hotmail.com>
2020-04-17 14:20:51 -0700

    initial commit

--- src/main/java/com/mohammadalsalkini/petclinic/SfgPetClinicApplication.java
+++ src/main/java/com/mohammadalsalkini/petclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.mohammadalsalkini.sfgpetclinic;
+package com.mohammadalsalkini.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
