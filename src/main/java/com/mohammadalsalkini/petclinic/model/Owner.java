cb5452840955c7d2e1d736c56e83d0cb5d341acc
Mohammad Salkini <eng.salkini@hotmail.com>
2020-04-17 14:38:06 -0700

    add POJOs #2

---
+++ src/main/java/com/mohammadalsalkini/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.mohammadalsalkini.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/mohammadalsalkini/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.mohammadalsalkini.petclinic.model;
+
+public class Vet extends Person{
+}
